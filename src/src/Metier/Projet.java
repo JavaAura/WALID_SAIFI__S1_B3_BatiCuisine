fc7cad81c6981650ae65dec40073586b757ebda0
WALIDSAIFI <saifiwalid21@gmail.com>
2024-09-20 11:43:42 +0100

    update.

--- src/src/Metier/Projet.java
+++ src/src/Metier/Projet.java
@@ -10,6 +10,54 @@ public class Projet{
     private Client client;
     private List<Composant> composants;
 
+    public String getNomProjet() {
+        return nomProjet;
+    }
+
+    public void setNomProjet(String nomProjet) {
+        this.nomProjet = nomProjet;
+    }
+
+    public List<Composant> getComposants() {
+        return composants;
+    }
+
+    public void setComposants(List<Composant> composants) {
+        this.composants = composants;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public double getCoutTotal() {
+        return coutTotal;
+    }
+
+    public void setCoutTotal(double coutTotal) {
+        this.coutTotal = coutTotal;
+    }
+
+    public double getMargeBeneficiaire() {
+        return margeBeneficiaire;
+    }
+
+    public void setMargeBeneficiaire(double margeBeneficiaire) {
+        this.margeBeneficiaire = margeBeneficiaire;
+    }
+
+    public String getEtatProjet() {
+        return etatProjet;
+    }
+
+    public void setEtatProjet(String etatProjet) {
+        this.etatProjet = etatProjet;
+    }
+
     public Projet(String nomProjet, List<Composant> composants, Client client, double coutTotal, double margeBeneficiaire) {
         this.nomProjet = nomProjet;
         this.composants = composants;
