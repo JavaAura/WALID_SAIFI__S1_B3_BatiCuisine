19dfe2917e201384a6a8bdf4f73930d74eacd6d2
WALIDSAIFI <saifiwalid21@gmail.com>
2024-09-20 11:43:15 +0100

    update.

--- src/src/Metier/Projet.java
+++ src/src/Metier/Projet.java
@@ -1,4 +1,23 @@
 package Metier;
 
+import java.util.List;
+
 public class Projet{
+    private String nomProjet;
+    private double margeBeneficiaire;
+    private double coutTotal;
+    private String etatProjet;
+    private Client client;
+    private List<Composant> composants;
+
+    public Projet(String nomProjet, List<Composant> composants, Client client, double coutTotal, double margeBeneficiaire) {
+        this.nomProjet = nomProjet;
+        this.composants = composants;
+        this.client = client;
+        this.etatProjet = "EN_COURS";
+        this.coutTotal = coutTotal;
+        this.margeBeneficiaire = margeBeneficiaire;
+    }
+
+
 }
