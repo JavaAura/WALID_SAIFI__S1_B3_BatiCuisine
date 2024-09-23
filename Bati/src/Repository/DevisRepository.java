430897d4c79be5919a7e91d8cb7725a7e9a74f4d
WALIDSAIFI <saifiwalid21@gmail.com>
2024-09-23 23:01:33 +0100

    update project

--- Bati/src/Repository/DevisRepository.java
+++ Bati/src/Repository/DevisRepository.java
@@ -1,4 +1,25 @@
 package Repository;
 
-public class DevisRepository {
+import Metier.Devis;
+
+public class DevisRepository implements  IDevisRepository{
+    @Override
+    public void ajouterDevis(Devis devis) {
+
+    }
+
+    @Override
+    public void mettreAJourAccordDevis(Devis devis) {
+
+    }
+
+    @Override
+    public Devis obtenirDevisParId(int id) {
+        return null;
+    }
+
+    @Override
+    public Devis trouverParProjetId(Long projetId) {
+        return null;
+    }
 }
