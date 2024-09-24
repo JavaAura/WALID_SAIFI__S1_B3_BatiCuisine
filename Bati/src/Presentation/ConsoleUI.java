df3b6432d99b545c88b5e47eb22dfb40eabf30cf
WALIDSAIFI <saifiwalid21@gmail.com>
2024-09-24 15:20:26 +0100

    update

--- Bati/src/Presentation/ConsoleUI.java
+++ Bati/src/Presentation/ConsoleUI.java
@@ -1,10 +1,52 @@
 package Presentation;
+import  java.util.*;
 
 public class ConsoleUI {
 
 
+    public void afficherMenu() {
+        Scanner scanner = new Scanner(System.in);
+        int choix;
+
+        do {
+            System.out.println("Menu:");
+            System.out.println("1. Créer un nouveau projet");
+            System.out.println("2. Afficher les projets existants");
+            System.out.println("3. Calculer le coût d'un projet");
+            System.out.println("4. Quitter");
+            System.out.print("Veuillez choisir une option (1-4): ");
+
+            choix = scanner.nextInt();
+
+            switch (choix) {
+                case 1:
+                    // Appel à la méthode pour créer un nouveau projet
+                   // creerNouveauProjet(scanner);
+                    break;
+                case 2:
+                    // Appel à la méthode pour afficher les projets existants
+                   // afficherProjetsExistants();
+                    break;
+                case 3:
+                    // Appel à la méthode pour calculer le coût d'un projet
+                   // calculerCoutProjet(scanner);
+                    break;
+                case 4:
+                    System.out.println("Merci d'avoir utilisé l'application. À bientôt!");
+                    break;
+                default:
+                    System.out.println("Choix invalide. Veuillez réessayer.");
+            }
+
+        } while (choix != 4);
+
+        scanner.close();
+    }
 
 
+
+
+    
 
 
 
